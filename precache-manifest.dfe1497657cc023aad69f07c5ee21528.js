b4f21145ca2f0cd4856541c3c4beff8e3e9a9aa5
Kamo <usera2955@gmail.com>
2020-07-07 10:19:47 +0300

    Updates

---
+++ precache-manifest.dfe1497657cc023aad69f07c5ee21528.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b38d25c1f1b228347eb6895d2dd5eb21",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "1d37c6fce1470d6694a3",
+    "url": "/react-todo-list/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "35b4d35c67c35ce77ba5",
+    "url": "/react-todo-list/static/css/main.71cc0ca5.chunk.css"
+  },
+  {
+    "revision": "1d37c6fce1470d6694a3",
+    "url": "/react-todo-list/static/js/2.def15403.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo-list/static/js/2.def15403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35b4d35c67c35ce77ba5",
+    "url": "/react-todo-list/static/js/main.1a4afe80.chunk.js"
+  },
+  {
+    "revision": "f5cf3106337617ffa6c9",
+    "url": "/react-todo-list/static/js/runtime-main.3dfc7ce8.js"
+  }
+]);
